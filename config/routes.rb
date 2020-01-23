546dda0f6dcf43405fac46f99602172535a6ccb6
タクミ <toldend@icloud.com>
2020-01-23 16:11:35 +0900

    users management function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "messages#index"
+  resources :users, only: [:edit, :update, :destroy]
+  # resources :messages, only: [:create, :show, :edit, :update]
 end
